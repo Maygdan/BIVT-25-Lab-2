6ecd0d33e0c1022c5c3e634d45178e32511ea32b
Maygdan <alesharet4253@gmail.com>
2025-10-05 04:11:09 +0300

    my change

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a29d85d0f9788534c795ff0c9a6d07d51a9d6c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1bef63ccf6226b20263ad9070329f7cbd8dd29f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
